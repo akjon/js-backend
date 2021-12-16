7ad4375d391f144bb4c33b2c5fa23a49a456c321
Johnny Karlsson <eshiak@gmail.com>
2021-12-16 17:15:44 +0100

    Added types and a name field requirement to Document model

--- models/Document.js
+++ models/Document.js
@@ -1,9 +1,18 @@
 const { Schema, model } = require("mongoose");
 
 const Document = new Schema({
-    _id: String,
-    name: String,
-    data: Object
+  _id: { type: String },
+  name: { type: String, required: true },
+  data: { type: Object },
+  creationDate: { type: Date, default: Date.now }
+});
+
+Document.pre('save', next => {
+  now = new Date();
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
 });
 
 module.exports = model("Document", Document);
